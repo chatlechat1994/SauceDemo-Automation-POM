1ccd0d67516f3f78e7f30c65f59566c25c4a8b97
chatlechat1994 <chatlechat1994@gmail.com>
2026-03-03 10:56:24 +0000

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,12 +3,12 @@ from selenium.webdriver.common.by import By
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
-        # 📍 The Map
+        # "MAP"
         self.username_field = (By.ID, "user-name")
         self.password_field = (By.ID, "password")
         self.login_button = (By.ID, "login-button")
 
-    # 🤚 The Actions (Make sure these are indented inside the class!)
+    
     def enter_username(self, username):
         self.driver.find_element(*self.username_field).send_keys(username)
 
